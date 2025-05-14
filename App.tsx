d011fbc682c2266ad3b5f8864369599cf6e18366
Yallayummy <yusuf@yallayummy.de>
2025-05-14 19:39:18 +0200

    Add files via upload
    Initial project upload

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="hero">
+        <h1>Yalla Yummy</h1>
+        <p>Orientalische Küche mit Herz ❤️</p>
+        <a className="cta-button" href="https://www.instagram.com/yallayummy.de" target="_blank">Jetzt auf Instagram ansehen</a>
+        <p className="contact">Kontakt: <a href="tel:015510410528">0155 10410528</a></p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
